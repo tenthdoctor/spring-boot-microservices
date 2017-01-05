f95705965a70c5bc1698047d27ba60f97cf9916a
The Doctor <doctorandabox@gmail.com>
2017-01-05 11:02:04 +0530

    sprint boot app with gradle

---
+++ boot-with-gradle/src/main/java/com/tenthdoctor/boot/StarterProjectGradleApplication.java
@@ -0,0 +1,12 @@
+package com.tenthdoctor.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterProjectGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterProjectGradleApplication.class, args);
+	}
+}
